b999f84de866cb6aad371007e6a58c432af2ffd7
gauss <gauss.lee@etraveli.com>
2018-01-24 18:55:48 +0800

    Added some parts of the event engine

--- event/event_engine.py
+++ event/event_engine.py
@@ -3,6 +3,7 @@ This file contains the event engine
 """
 from threading import Thread
 from queue import Queue
+from collections import defaultdict
 
 
 class StandardEventEngine(object):
@@ -20,6 +21,54 @@ class StandardEventEngine(object):
         # event handling thread
         self._event_thread = Thread(target=self._run)
 
+        # strategy bucket
+        self.strategy_bucket = defaultdict()
 
     def _run(self):
         pass
+
+    def _process(self):
+        pass
+
+    def stop(self):
+        """
+        used to stop the whole event engine
+        """
+        self._on = False
+        self._event_thread.join()
+
+    def register_strategy(self, strategy, name):
+        """
+        A strategy is a class which at least includes OnBar or OnTick method.
+        :param strategy: a strategy calss
+        :return:
+        """
+        if hasattr(strategy, "OnBar"):
+            pass
+        elif hasattr(strategy, "OnTick"):
+            pass
+        else:
+            raise ValueError("Invalid strategy")
+        assert isinstance(name, str), "strategy name must be string"
+        if name in self.strategy_bucket:
+            raise KeyError("%s strategy already registered")
+        self.strategy_bucket[name] = strategy
+
+    def unregister_strategy(self, name):
+        """
+        remove the strategy with specified name
+        :param name:
+        :return:
+        """
+        assert isinstance(name, str), "target strategy name must be string"
+        if name in self.strategy_bucket:
+            self.strategy_bucket.pop(name)
+
+    def insert_event(self, event):
+        self._event_queue.put(event)
+
+
+
+
+
+
